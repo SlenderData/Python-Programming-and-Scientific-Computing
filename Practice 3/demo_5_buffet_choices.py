5fc6032cfbb9620a6731bb2330bc722b3ce79fd2
SlenderData <mayunji2001@outlook.com>
2024-03-29 19:46:12 +0800

    Add Practice 3

--- Practice 3/demo_5_buffet_choices.py
+++ Practice 3/demo_5_buffet_choices.py
@@ -3,15 +3,15 @@
 # 自助餐
 
 # 初始菜单
-foods = ('披萨', '炸鸡', '汉堡', '沙拉', '冰淇淋')
+foods = ('披萨🍕', '炸鸡🍗', '汉堡🍔', '沙拉🥗', '冰淇淋🍦')
 for food in foods:
     print(food)
 
 # 尝试修改其中一个元素
-# foods[0] = '寿司' # 这会引发TypeError
+# foods[0] = '寿司🍣' # 这会引发TypeError
 
 # 新菜单
-foods = ('寿司', '炸鸡', '汉堡', '沙拉', '果冻')
+foods = ('寿司🍣', '炸鸡🍗', '汉堡🍔', '沙拉🥗', '布丁🍮')
 print("\n新菜单：")
 for food in foods:
     print(food)
